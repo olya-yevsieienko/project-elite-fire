4e91dd645929dfabd003c0e23fffade4046c5c86
Olya Yevsieienko <olya.evsnko@gmail.com>
2023-02-24 21:50:33 +0300

    Created Footer, Navigation, Mobile Menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+'use strict';
+
 import './styles/style.scss';
 import './script/slider';
+
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
